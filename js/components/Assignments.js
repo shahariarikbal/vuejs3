eff27725a47388a3ca65275ab19ae8b4dc59a5ba
shahariarikbal <shahariar.ikbal86@gmail.com>
2023-04-12 01:13:21 +0100

    vue3 project

---
+++ js/components/Assignments.js
@@ -0,0 +1,27 @@
+import AssignmentList from "./AssignmentList.js";
+export default {
+    components: { AssignmentList },
+    template: `
+        <assignment-list :assignments="filter.inProgress" title="In Progress"></assignment-list>
+        <assignment-list :assignments="filter.completed" title="Completed"></assignment-list>
+    `,
+
+    data(){
+        return {
+            assignments: [
+                { name: 'Finish Project', complete: false, id: 1 },
+                { name: 'Read Chapter 4', complete: false, id: 2 },
+                { name: 'Turn in homework', complete: false, id: 3 },
+            ]
+        }
+    },
+
+    computed: {
+        filter(){
+            return {
+                inProgress:this.assignments.filter(assignment => !assignment.complete),
+                completed:this.assignments.filter(assignment => assignment.complete)
+            }
+        }
+    }
+}
